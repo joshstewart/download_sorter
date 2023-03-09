4d0d53ef220581e17979d19e9dac6e17f3d19f17
Josh Stewart <josh@rocketsofawesome.com>
2023-03-09 14:47:14 -0600

    Make sure the metadata we're checking aginast is a url

--- download_sorter.py
+++ download_sorter.py
@@ -1,36 +1,37 @@
-#!/usr/bin/env python
-
-import argparse
 import os
 import shutil
+import subprocess
+import re
+import argparse
 
-parser = argparse.ArgumentParser(description='Move files to directories based on domain matching.')
-parser.add_argument('downloads_dir', metavar='DOWNLOADS_DIR', type=str,
-                    help='the path to the Downloads folder')
-parser.add_argument('--rules', metavar='RULES_FILE', type=str, default=None,
-                    help='the path to a file containing rules for domain matching')
-
+parser = argparse.ArgumentParser(description='Sort files based on their URLs.')
+parser.add_argument('source_dir', type=str, help='path to directory containing files to be sorted')
+parser.add_argument('domain_rules', type=str, help='path to file containing domain rules')
 args = parser.parse_args()
 
-# Set the path to the Downloads folder
-downloads_dir = os.path.expanduser(args.downloads_dir)
+# Define dictionary of domain rules
+domain_dirs = {}
+with open(args.domain_rules, 'r') as f:
+    for line in f:
+        domain, directory = line.strip().split(":")
+        domain_dirs[domain] = directory
 
-# Define rules for moving files to directories based on domain matching
-rules = {}
-if args.rules:
-    with open(args.rules, 'r') as f:
-        for line in f:
-            domain, dest_dir = line.strip().split(':')
-            rules[domain] = dest_dir
-
-for filename in os.listdir(downloads_dir):
-    file_path = os.path.join(downloads_dir, filename)
-    if os.path.isfile(file_path):
-        # Get the URL from where the file was downloaded
-        url = os.popen(f"mdls -name kMDItemWhereFroms '{file_path}'").read()
-        if url:
-            url = url.strip().strip('()"')  # Clean up the URL string
-            domain = url.split("//")[-1].split("/")[0]  # Extract the domain from the URL
-            if domain in rules:
-                dest_dir = rules[domain]
-                shutil.move(file_path, os.path.join(dest_dir, filename))  # Move the file to the destination directory
+# Loop through files in source directory
+for filename in os.listdir(args.source_dir):
+    filepath = os.path.join(args.source_dir, filename)
+    
+    # Use mdls to get kMDItemWhereFroms metadata for file
+    mdls_output = subprocess.check_output(["mdls", "-name", "kMDItemWhereFroms", filepath]).decode("utf-8")
+    
+    # Use regular expressions to extract URLs from kMDItemWhereFroms metadata
+    urls = re.findall(r"https?://[^\s]+", mdls_output)
+    
+    # Loop through URLs and check if they match one of the domains in the list
+    for url in urls:
+        domain = url.split("/")[2]
+        if domain in domain_dirs:
+            # If a match is found, move the file to the corresponding directory
+            dest_dir = domain_dirs[domain]
+            print(f'Moving {filename} to {dest_dir}')
+            shutil.move(filepath, os.path.join(dest_dir, filename))
+            break  # Stop checking URLs once the file has been moved
